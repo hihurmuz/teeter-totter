bba8493f90b06eee49909aa2af381f35b7da2cbc
halil <hihurmuz@gmail.com>
2022-03-28 01:32:15 +0300

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="app">
+    <v-main>
+      <TheHeader />
+    </v-main>
+    <GameArea />
+
+  </v-app>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import GameArea from "./components/GameArea.vue";
+
+export default {
+  components: {
+    TheHeader,
+    GameArea,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+</style>
