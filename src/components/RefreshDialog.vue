2f69e02789db4a88a1de9f0f711987053c0d08d8
halil <hihurmuz@gmail.com>
2022-03-28 01:51:15 +0300

    feat: refresh game dialog create

---
+++ src/components/RefreshDialog.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-dialog
+    v-model="isModalShown"
+    width="500"
+  >
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2">
+        Teeter Totter Game
+      </v-card-title>
+
+      <v-img src="https://cdn4.iconfinder.com/data/icons/carnival-vol-3/48/116-512.png"></v-img>
+
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          color="primary"
+          text
+          @click="initGame"
+        >
+          Refresh The Game
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name : 'RefreshDialog',
+  data(){
+    return {
+      dialog: false
+    }
+  },
+  computed: {
+    ...mapState(["isModalShown"]),
+  },
+  methods: {
+    ...mapActions(["initGame"]),
+  },
+};
+</script>
