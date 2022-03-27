991129e63bdee47b499979b192460598e8801539
halil <hihurmuz@gmail.com>
2022-03-28 01:50:46 +0300

    feat: game area wrapper crate

---
+++ src/components/GameArea.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="game-area">
+    <falling-area></falling-area>
+    <teeter-totter></teeter-totter>
+  </main>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+import FallingArea from './FallingArea.vue';
+import TeeterTotter from './TeeterTotter.vue';
+
+export default {
+  components : {
+    FallingArea,
+    TeeterTotter
+  },
+  created() {
+    this.initGame();
+  },
+  methods : {
+    ...mapActions(['initGame'])
+  }
+};
+</script>
