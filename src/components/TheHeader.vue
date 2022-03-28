100080b9a92b7c90f6413228a8c9ec0e5f550671
halil <hihurmuz@gmail.com>
2022-03-28 03:34:41 +0300

    feat: i18n add

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,37 +2,61 @@
   <header class="the-header">
 
     <div>
-      <v-btn  outlined @click="toggleSimulation">
+      <v-btn class="mr-4" outlined @click="toggleSimulation">
         <v-icon>{{ isGamePaused ? 'mdi-play' : 'mdi-pause' }}</v-icon>
       </v-btn>
       <v-btn  outlined @click="initGame">
         <v-icon>mdi-refresh</v-icon>
       </v-btn>
     </div>
-    <div class="the-objects-stats">
-      <div>
-        Total weight:
-        <span class="the-objects-stats__weight"> {{ totalstandingObjectsWeight }} kg </span>
+
+    <div class="d-flex flex-row " v-if="this.$vuetify.breakpoint.width >= 600">
+      <div class="the-objects-stats">
+        <div>
+          {{ $t('totalWeight') }}:
+          <span class="the-objects-stats__weight"> {{ totalstandingObjectsWeight }} kg </span>
+        </div>
+        <div>
+          {{ $t('momentum') }}:
+          <span class="the-objects-stats__weight"> {{ fallingObject[0] && fallingObject[0].weight }} kg </span>
+        </div>
       </div>
-      <div>
-        Momentum:
-        <span class="the-objects-stats__weight"> {{ fallingObject[0] && fallingObject[0].weight }} kg </span>
-      </div>
-    </div>
-    <div class="the-objects-stats">
-      <div>
-        Total weight:
-        <span class="the-objects-stats__weight"> {{ totalRandomObjectsWeight }} kg </span>
-      </div>
-      <div>
-        Momentum:
-        <span class="the-objects-stats__weight"> {{ randomLastObjectWeight }} kg </span>
+
+      <div class="the-objects-stats">
+        <div>
+          {{ $t('totalWeight') }}:
+          <span class="the-objects-stats__weight"> {{ totalRandomObjectsWeight }} kg </span>
+        </div>
+        <div>
+          {{ $t('momentum') }}:
+          <span class="the-objects-stats__weight"> {{ randomLastObjectWeight }} kg </span>
+        </div>
       </div>
     </div>
 
-    <div>
-      lorem
-    </div>
+
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          text
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-translate</v-icon>
+          <v-icon>mdi-chevron-down</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in language"
+          :key="index"
+          @click="changeLanguage(item.symbol)"
+        >
+          <v-img :src="item.url" width="40" />
+          <v-list-item-title class="ml-2">{{ item.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
     <refresh-dialog />
   </header>
 </template>
@@ -43,6 +67,22 @@ import RefreshDialog from '@/components/RefreshDialog';
 
 export default {
   components : { RefreshDialog },
+  data() {
+    return {
+      language: [
+        {
+          symbol: 'tr',
+          text: 'Türkçe',
+          url: 'https://countryflagsapi.com/svg/tr'
+        },
+        {
+          symbol: 'en',
+          text: 'English',
+          url: 'https://countryflagsapi.com/svg/us'
+        }
+      ]
+    }
+  },
   computed: {
     ...mapState(["standingObjects", "fallingObject", "isGamePaused", "isModalShown", "isRandomObjects"]),
 
@@ -69,6 +109,10 @@ export default {
 
     getTotalWeight(objects = []) {
       return objects.reduce((total, current) => (total += current.weight), 0);
+    },
+
+    changeLanguage(lang) {
+      this.$i18n.locale = lang;
     },
 
     handleSpaceClick(event) {
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from "vue";
 import vuetify from './plugins/vuetify'
+import i18n from './i18n'
 import App from "./App.vue";
 import store from "./store";
+
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
   vuetify,
+  i18n,
   render: (h) => h(App),
 }).$mount("#app");
